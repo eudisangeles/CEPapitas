3a3eb29c083d2af8cde148a9eddca1bf55b5311f
fer <vfernando89@hotmail.com>
2017-08-03 19:20:38 -0500

    v3
    final

--- CERomerin/Controllers/HomeController.cs
+++ CERomerin/Controllers/HomeController.cs
@@ -8,10 +8,32 @@ namespace CERomerin.Controllers
 {
     public class HomeController : Controller
     {
+        private Models.ComercioBDEntities bd = new Models.ComercioBDEntities();
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(string id = "")
         {
-            return View();
+
+
+            //Logica de acceso a base de datos
+            var productos = bd.Producto.Where(x => x.Denominacion.Contains(id))
+                .Take(4)
+                .ToList();
+            //Devolver una lista de Productos
+            ViewBag.clave = id;
+            return View(productos);
+       
+        }
+
+
+    public ActionResult Buscar(string id = "")
+        {
+            //Logica de acceso a base de datos
+            var productos = bd.Producto.Where(x => x.Denominacion.Contains(id))
+                .Take(12)
+                .ToList();
+            //Devolver una lista de Productos
+            ViewBag.clave = id;
+            return View(productos);
         }
     }
 }
